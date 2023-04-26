f348e9997d36514a9d939584a9f7fae431a308c6
morinoko <morinoko@hey.com>
2023-04-26 12:48:31 +0000

    Add small test program

--- app/main.cpp
+++ app/main.cpp
@@ -6,7 +6,28 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    cout << "Hello world!" << endl;
+    Trie trie;
+
+    trie.Insert("apple");
+    trie.Insert("cat");
+    trie.Insert("bark");
+    trie.Insert("applesauce");
+    trie.Insert("catepillar");
+    trie.Insert("catacomb");
+    trie.Insert("catch");
+    trie.Insert("zebra");
+
+    cout << "All words in Trie:" << endl;
+    trie.Print();
+
+    cout << endl;
+    cout << "Suggestions for prefix 'cat':" << endl;
+
+    vector<string> suggestions = trie.SuggestionsForPrefix("cat");
+
+    for (auto suggestion : suggestions) {
+        cout << suggestion << endl;
+    }
     
     return 0;
 }
